0fcd2f4328f8fd092b8a9f8d5c67e36dd3f29fa9
wei chen <wei chen>
2017-03-01 17:02:07 +0800

    add order print demo

--- demo/src/main/java/com/shuwang/test/Application.java
+++ demo/src/main/java/com/shuwang/test/Application.java
@@ -1,9 +1,13 @@
 package com.shuwang.test;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import com.shuwang.config.ShuwangCloudConfig;
 import com.shuwang.test.model.Order;
+import com.shuwang.test.model.OrderItem;
 import com.shuwang.test.service.OrderPrintService;
 
 public class Application {
@@ -15,10 +19,33 @@ public class Application {
 		long orderid = System.currentTimeMillis();
 		int devid = 12039;
 		Order order = new Order();
+		order.setContactAddr("测试地址");
+		order.setContactName("数网");//名字
+		order.setContactPhone("666666");//手机
+		order.setCreateTime(new Date());//时间
+		order.setOrderId(orderid);//订单号
+		order.setOrderMemo("测试订单请查收");//备注
+		order.setOrderSeq(1);//第几单
+		order.setShopName("测试店铺");//店铺名称
+		order.setTotalAmount("20");//总金额
+		
+		OrderItem orderItem=new OrderItem();
+		orderItem.setAmount("20");//菜品金额
+		orderItem.setName("鸡腿饭");//菜品
+		orderItem.setQuantity(1);//菜品数量
+		List<OrderItem>orderItems=new ArrayList<OrderItem>();
+		orderItems.add(orderItem);
+		order.setItems(orderItems);//菜品list集合
+		
 		//
 		OrderPrintService orderPrintService = new OrderPrintService();
 		try {
-			orderPrintService.printOrder(orderid, devid, order);
+			boolean success = orderPrintService.printOrder(orderid, devid, order);
+			if (success) {
+				System.out.println("success");
+			} else {
+				System.out.println("fail");
+			}
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
