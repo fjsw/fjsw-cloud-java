f70a620b0a3777e7e8b05438f6627a9d7e09188c
fjswchenw <chenw@shuwang.info>
2017-05-10 12:43:07 +0800

    增加修改订单状态接口，并升级版本到v1.0.6

--- shuwang/src/main/java/com/shuwang/cloud/service/CloudPrintService.java
+++ shuwang/src/main/java/com/shuwang/cloud/service/CloudPrintService.java
@@ -15,6 +15,7 @@ public class CloudPrintService {
 
 	/**
 	 * check whether initialized
+	 * 
 	 * @return boolean
 	 */
 	public boolean isInitialized() {
@@ -23,28 +24,44 @@ public class CloudPrintService {
 
 	/**
 	 * initial service
-	 * @param appid 应用编号
-	 * @param appsecret 应用秘钥
-	 * @param gatewayUrl 接口网关
+	 * 
+	 * @param appid
+	 *            应用编号
+	 * @param appsecret
+	 *            应用秘钥
+	 * @param gatewayUrl
+	 *            接口网关
 	 */
-	public void initial(String appid, String appsecret,String gatewayUrl) {
+	public void initial(String appid, String appsecret, String gatewayUrl) {
 		this.appid = appid;
 		this.appsecret = appsecret;
 		this.gatewayUrl = gatewayUrl;
 	}
 
-	public boolean sendPrint(long printid, int devid, String title, String printStream, int mode) {
+	/**
+	 * 向设备发送订单打印
+	 * @param printid 打印编号(订单编号)
+	 * @param devid 设备编号
+	 * @param title 标题
+	 * @param printStream 打印流
+	 * @param mode 打印模式
+	 * @return
+	 */
+	public boolean sendPrint(long printid, int devid, String title,
+			String printStream, int mode) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("method", "print.cloud.text");
 		params.put("appid", appid);
-		params.put("timestamp", String.valueOf(System.currentTimeMillis()/1000));
+		params.put("timestamp",
+				String.valueOf(System.currentTimeMillis() / 1000));
 		params.put("printid", String.valueOf(printid));
 		params.put("devid", String.valueOf(devid));
 		params.put("mode", String.valueOf(mode));
-		if (title!=null && !title.isEmpty()) {
+		if (title != null && !title.isEmpty()) {
 			params.put("title", title);
 		}
-		String signature = GatewayProtocolService.signRequest(params, appsecret);
+		String signature = GatewayProtocolService
+				.signRequest(params, appsecret);
 		params.put("sign", signature);
 		params.put("printstream", printStream);
 		String result = GatewayProtocolService.callDirect(params, gatewayUrl);
@@ -52,17 +69,30 @@ public class CloudPrintService {
 		return true;
 	}
 
-	public boolean sendPrint(long printid, int devid, String title, String printStream, Integer mode, Integer ordermode, Integer timeout) {
+	/**
+	 * 向设备发送订单打印
+	 * @param printid 打印编号(订单编号)
+	 * @param devid 设备编号
+	 * @param title 标题
+	 * @param printStream 打印流
+	 * @param mode 打印模式
+	 * @param ordermode 订单状态
+	 * @param timeout 打印超时时间
+	 * @return boolean
+	 */
+	public boolean sendPrint(long printid, int devid, String title,
+			String printStream, Integer mode, Integer ordermode, Integer timeout) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("method", "print.cloud.text");
 		params.put("appid", appid);
-		params.put("timestamp", String.valueOf(System.currentTimeMillis()/1000));
+		params.put("timestamp",
+				String.valueOf(System.currentTimeMillis() / 1000));
 		params.put("printid", String.valueOf(printid));
 		params.put("devid", String.valueOf(devid));
 		if (mode != null) {
 			params.put("mode", String.valueOf(mode));
 		}
-		if (title!=null && !title.isEmpty()) {
+		if (title != null && !title.isEmpty()) {
 			params.put("title", title);
 		}
 		if (ordermode != null) {
@@ -71,11 +101,31 @@ public class CloudPrintService {
 		if (timeout != null) {
 			params.put("timeout", String.valueOf(timeout));
 		}
-		String signature = GatewayProtocolService.signRequest(params, appsecret);
+		String signature = GatewayProtocolService
+				.signRequest(params, appsecret);
 		params.put("sign", signature);
 		params.put("printstream", printStream);
 		String result = GatewayProtocolService.callDirect(params, gatewayUrl);
 		log.debug("sendPrint() result={}", result);
+		return true;
+	}
+
+	/**
+	 * 修改设备订单状态
+	 * @param printid 打印编号(订单编号)
+	 * @param devid 设备编号
+	 * @param orderstatus 订单状态
+	 * @return boolean
+	 */
+	public boolean changeOrderStatus(long printid, int devid, int orderstatus) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("method", "order.status.change");
+		params.put("printid", String.valueOf(printid));
+		params.put("devid", String.valueOf(devid));
+		params.put("orderstatus", orderstatus);
+		String result = GatewayProtocolService.callMethod(params, appid,
+				appsecret, gatewayUrl);
+		log.debug("changeOrderStatus() result={}", result);
 		return true;
 	}
 }
--- shuwang/src/main/java/com/shuwang/cloud/service/GatewayProtocolService.java
+++ shuwang/src/main/java/com/shuwang/cloud/service/GatewayProtocolService.java
@@ -1,6 +1,5 @@
 package com.shuwang.cloud.service;
 
-
 import com.google.gson.Gson;
 import com.shuwang.cloud.util.HmacMd5Util;
 
@@ -10,21 +9,56 @@ import org.slf4j.LoggerFactory;
 import java.util.*;
 
 public class GatewayProtocolService {
-	protected static final Logger log = LoggerFactory.getLogger(GatewayProtocolService.class);
+	protected static final Logger log = LoggerFactory
+			.getLogger(GatewayProtocolService.class);
 	private static HttpsService http = new HttpsService();
 	private static Gson gson = new Gson();
 
-	public static String callMethod(Map<String, Object> params,String appid,String appsecret,String gatewayUrl) {
+	/**
+	 * 通过业务参数调用接口, 方法内部进行时间戳和签名运算
+	 * @param params 业务参数
+	 * @param appid 应用编号
+	 * @param appsecret 应用密钥
+	 * @param gatewayUrl 网关接口地址
+	 * @return
+	 */
+	public static String callMethod(Map<String, Object> params, String appid,
+			String appsecret, String gatewayUrl) {
 		params.put("appid", appid);
-		params.put("timestamp", String.valueOf(System.currentTimeMillis()/1000));
+		params.put("timestamp",
+				String.valueOf(System.currentTimeMillis() / 1000));
 		// sign
-		String signature = signRequest(params,appsecret);
+		String signature = signRequest(params, appsecret);
 		params.put("sign", signature);
 		// call
-		return callDirect(params,gatewayUrl);
+		return callDirect(params, gatewayUrl);
 	}
 
-	public static String signRequest(Map<String, Object> params,String appsecret) {
+	/**
+	 * 直接调用接口, 需要在外部计算好时间戳和签名
+	 * @param params 构造好的请求参数
+	 * @param gatewayUrl 网关接口地址
+	 * @return
+	 */
+	public static String callDirect(final Map<String, Object> params, String gatewayUrl) {
+		String jsonObject = gson.toJson(params);
+		try {
+			String text = http.jsonPost(gatewayUrl, jsonObject);
+			return text;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return e.getMessage();
+		}
+	}
+
+	/**
+	 * 计算签名
+	 * @param params 业务参数
+	 * @param appsecret 应用密钥
+	 * @return
+	 */
+	public static String signRequest(Map<String, Object> params,
+			String appsecret) {
 		// sort keys
 		List<Map.Entry<String, Object>> list = new ArrayList<Map.Entry<String, Object>>();
 		list.addAll(params.entrySet());
@@ -32,13 +66,13 @@ public class GatewayProtocolService {
 		Collections.sort(list, kc);
 		// 把所有参数名和参数值串在一起
 		StringBuilder query = new StringBuilder();
-		for (Iterator<Map.Entry<String, Object>> it = list.iterator(); it.hasNext();) {
+		for (Iterator<Map.Entry<String, Object>> it = list.iterator(); it
+				.hasNext();) {
 			Map.Entry<String, Object> item = it.next();
 			Object value = item.getValue();
 			if (value == null) {
 				;
-			}
-			else if (value instanceof String) {
+			} else if (value instanceof String) {
 				String string_value = (String) value;
 				if (!string_value.isEmpty()) {
 					query.append(item.getKey()).append(string_value);
@@ -57,15 +91,22 @@ public class GatewayProtocolService {
 		}
 		return sign;
 	}
-	
-	public static boolean checkSignEqual(Map<String, Object> data,String appsecret) {
+
+	/**
+	 * 校验签名是否一致
+	 * @param data 原始数据
+	 * @param appsecret 应用密钥
+	 * @return boolean
+	 */
+	public static boolean checkSignEqual(Map<String, Object> data,
+			String appsecret) {
 		String sign = (String) data.get("sign");
 		if (sign == null || sign.isEmpty()) {
 			return false;
 		}
 		//
 		data.remove("sign");
-		String signRequest = signRequest(data,appsecret);
+		String signRequest = signRequest(data, appsecret);
 		return sign.equals(signRequest);
 	}
 
@@ -74,17 +115,6 @@ public class GatewayProtocolService {
 		public int compare(Map.Entry<String, Object> m,
 				Map.Entry<String, Object> n) {
 			return m.getKey().compareTo(n.getKey());
-		}
-	}
-	
-	public static String callDirect(final Map<String, Object> params,String url) {
-		String jsonObject = gson.toJson(params);
-		try {
-			String text = http.jsonPost(url,jsonObject);
-			return text;
-		}catch (Exception e){
-			e.printStackTrace();
-			return e.getMessage();
 		}
 	}
 }
